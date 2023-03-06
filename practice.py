930123ae52df148d25c8837221143f43fda076a1
jeondoguen <wjsehrbs2632@naver.com>
2023-03-06 18:25:26 +0900

    practice dfs

--- practice.py
+++ practice.py
@@ -1,10 +1,26 @@
-h = int(input())
-count = 0
+def dfs(x, y):
+    if x <= -1 or x>= n or y <= -1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1 # 해당 노드 방문 처리
+        dfs(x-1, y) # 상하좌우 방문
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
 
-for i in range(h+1):
-    for j in range(60):
-        for k in range(60):
-            if '3' in str(i) + str(j) + str(k):
-                count += 1
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
 
-print(count)
+
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True: # 현재 위치에서 DFS 수행
+            result += 1
+
+print(result)
