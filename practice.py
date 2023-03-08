8359c643e07322dee6145d6a4c77c37878396f26
jeondoguen <wjsehrbs2632@naver.com>
2023-03-08 21:49:42 +0900

    binary_search

--- practice.py
+++ practice.py
@@ -1,32 +1,38 @@
-# # 이진 탐색 소스 코드
-# def binary_search(array, target, start, end):
-#     if start > end:
-#         return None
-#     mid = (start + end) / 2
-#     # 찾은 경우 중간인덱스 반환
-#     if array[mid] == target:
-#         return mid
-#     # 중간인덱스의 값이 타겟보다 클 경우 -> 왼쪽까지의 값을 다시 이진 탐색
-#     elif array[mid] > target:
-#         return binary_search(array, target, start, mid - 1)
-#     # 중간인덱스의 값이 타겟보다 작을 경우 -> 중간인덱스의 오른쪽을 시작점으로 다시 이진 탐색
-#     else:
-#         return binary_search(array, target, mid + 1, end)
-    
-# n, target = list(map(int, input().split()))
-# array = list(map(int, input().split()))
+# # 최대 길이를 제외한 그 다음 길이부터 생각해서 잘랐을 때 길이가 손님이 원하는 길이보다 작으면 다음 원소 비교
+# n, m = map(int, input().split()) 
+# array = list(map(int, input().split())) 
 
-# result = binary_search(array, target, 0, n-1)
-# if result == None:
-#     print("원소가 존재하지 않습니다.")
-# else:
-#     print(result + 1)
+# # 시작점과 끝점
+# start = 0
+# end = max(array)
 
-# 파이썬 이진탐색 라이브러리
+# result = 0
+# while(start <= end):
+#     total = 0
+#     mid = (start + end) // 2
+#     for x in array:
+#         if x > mid:
+#             total += x - mid # 잘랐을 때 떡의 양 계산
+#     # 떡의 길이가 부족한 경우, 왼쪽 부분 탐색
+#     if total < m:
+#         end = mid - 1
+#     else: # 떡의 양이 충분한 경우
+#         result = mid # 현재 만족한 상태에서의 값을 저장
+#         start = mid + 1 # 최댓값이 있는지 오른쪽 부분 탐색
+
+# print(result)
 from bisect import bisect_left, bisect_right
 
-a = [1, 2, 4, 4, 8]
-x = 4
+def count_by_range(array, left_value, right_value):
+    right_index = bisect_right(array, right_value)
+    left_index = bisect_left(array, left_value)
+    return right_index - left_index
 
-print(bisect_left(a, x))
-print(bisect_right(a, x))
+n, x = map(int, input().split())
+array = list(map(int, input().split()))
+
+count = count_by_range(array, x, x)
+if count == 0:
+    print(-1)
+else:
+    print(count)
