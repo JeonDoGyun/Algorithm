0b31ddeaf0dd3e4f270d144897d1aa2219732e5b
jeondoguen <wjsehrbs2632@naver.com>
2023-03-15 16:56:16 +0900

    Kruskal Algorithm

--- practice.py
+++ practice.py
@@ -466,3 +466,79 @@
 # for i in range(1, v+1):
 #     print(parent[i], end=' ')
 
+# 서로소를 이용한 사이클 판별 알고리즘
+# def find_parent(parent, x):
+#     # 루트 노드를 찾을 때까지 호출
+#     if parent[x] != x:
+#         parent[x] = find_parent(parent, parent[x])
+#     return parent[x]
+
+# def union_parent(parent, a, b):
+#     a = find_parent(parent, a)
+#     b = find_parent(parent, b)
+#     if a < b:
+#         parent[b] = a
+#     else:
+#         parent[a] = b
+
+# v, e = map(int, input().split())
+# parent = [0] * (v+1)
+
+# for i in range(1, v+1):
+#     parent[i] = i
+
+# cycle = False
+# for i in range(e):
+#     a, b = map(int, input().split())
+#     # 사이클이 발생한 경우
+#     if find_parent(parent, a) == find_parent(parent, b):
+#         cycle = True
+#         break
+#     else:
+#         union_parent(parent, a, b)
+
+# if cycle:
+#     print("Cycle 발생")
+# else:
+#     print("Cycle 없음")
+
+# 크루스칼 알고리즘
+def find_parent(parent, x):
+    # 루트 노드를 찾을 때까지 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+
+# 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
+edges = []
+result = 0
+
+for i in range(1, v+1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+# 간선의 비용을 기준으로 오름차순 정렬
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+	# 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(cost)
